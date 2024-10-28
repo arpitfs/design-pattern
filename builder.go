01bc2ff0b731d92d70407c5f673d4297e55238f6
unknown <amarpitmalik204@gmail.com>
2024-10-28 23:02:17 +0530

    builder pattern

---
+++ builder.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type User struct {
+	name  string
+	email string
+}
+
+type UserBuilder struct {
+	user User
+}
+
+func (u *UserBuilder) SetEmail(email string) *UserBuilder {
+	u.user.email = email
+	return u
+}
+
+func (u *UserBuilder) SetName(name string) *UserBuilder {
+	u.user.name = name
+	return u
+}
+
+func NewUserBuilder() *UserBuilder {
+	return &UserBuilder{}
+}
+
+func (u *UserBuilder) Build() User {
+	return u.user
+}
+
+func main() {
+	user := NewUserBuilder().
+		SetEmail("myEMail").
+		SetName("myName").
+		Build()
+
+	fmt.Println(user)
+}
